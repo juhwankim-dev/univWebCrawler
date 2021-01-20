5b62297725c57de951bc1f2c085c9b6a3b91793e
juhwankim-dev <76620764+juhwankim-dev@users.noreply.github.com>
2021-01-20 12:04:40 +0900

    Update: heroku server, hide important keys
    I moved server my laptop to heroku.
    it won't show crucial keys on code anymore. hided it in environment var

--- parsingPost.py
+++ parsingPost.py
@@ -1,3 +1,5 @@
+from collections import OrderedDict
+
 from selenium import webdriver
 import datetime
 from pyfcm import FCMNotification
@@ -7,47 +9,42 @@ from firebase_admin import db
 import inko
 myInko = inko.Inko()
 import os
+import json
+
+# Firebase database 인증을 위해 ... json 파일을 heroku에 업로드할 수 없기 때문
+cred_json = OrderedDict()
+cred_json["type"] = os.environ["type"]
+cred_json["project_id"] = os.environ["project_id"]
+cred_json["private_key_id"] = os.environ["private_key_id"]
+cred_json["private_key"] = os.environ["private_key"]
+cred_json["client_email"] = os.environ["client_email"]
+cred_json["client_id"] = os.environ["client_id"]
+cred_json["auth_uri"] = os.environ["auth_uri"]
+cred_json["token_uri"] = os.environ["token_uri"]
+cred_json["auth_provider_x509_cert_url"] = os.environ["auth_provider_x509_cert_url"]
+cred_json["client_x509_cert_url"] = os.environ["client_x509_cert_url"]
+JSON = json.dumps(cred_json)
+JSON = json.loads(JSON)
 
 # 링크, 키값 등
 APIKEY = os.environ["APIKEY"]
-JSON_LOCATION = os.environ["JSON_LOCATION"]
+DRIVE_LOCATION = '/app/.chromedriver/bin/chromedriver'
 SITE_URL = "http://www.anyang.ac.kr/bbs/board.do?menuId=23&bsIdx=61&bcIdx=20"
-DRIVE_LOCATION = "C:\chromedriver\chromedriver.exe"
 XPATH = '//*[@id="boardList"]/tbody/tr[6]/td[1]' # 가장 최근에 올라온 게시글의 번호
 
 #
-cred = credentials.Certificate(JSON_LOCATION)
+cred = credentials.Certificate(JSON)
 firebase_admin.initialize_app(cred,{
-    'databaseURL' : 'https://pushnotification-6e716-default-rtdb.firebaseio.com/'
+    'databaseURL' : os.environ["databaseURL"]
 })
 
 # 파이어베이스 콘솔에서 얻어 온 API키를 넣어 줌
 push_service = FCMNotification(api_key=APIKEY)
 
-numberFile = open('lastPostNum.txt', mode='wt', encoding='utf-8')
-Log = open('Log.txt', mode='at', encoding='utf-8')
-
 #초기 셋팅
 options = webdriver.ChromeOptions()
 options.add_argument('headless')
 chrome_options = options
-driver = webdriver.Chrome(DRIVE_LOCATION, options=options)
-while(True):
-    try:
-        driver.get(SITE_URL)
-        driver.implicitly_wait(time_to_wait=5)
-        break;
-    except:
-        now = datetime.datetime.now()
-        Log.write("TIMED_OUT_ERROR(Occurrence Time): " + now.isoformat() + "\n")
-
-element = driver.find_element_by_xpath(XPATH)
-lastPostNum = element.text  # 가장 최근에 올라온 게시물의 번호
-
-numberFile.write(lastPostNum)
-numberFile.close()
-Log.close()
-driver.close()
 
 def importSubscribedKeyword():
     keywords = []
@@ -68,10 +65,9 @@ def sendMessage(title, keyword):
     keyword = myInko.ko2en(keyword)
     # 구독한 사용자에게만 알림 전송
     result = push_service.notify_topic_subscribers(topic_name=keyword, data_message=data_message)
-
     print(result)
 
-def activateBot(Log) :
+def activateBot(lastPostNum) :
     driver = webdriver.Chrome(DRIVE_LOCATION, options=options)
     while (True):
         try:
@@ -80,11 +76,7 @@ def activateBot(Log) :
             break;
         except:
             now = datetime.datetime.now()
-            Log.write("TIMED_OUT_ERROR(Occurrence Time): " + now.isoformat() + "\n")
-
-    numberFile = open('lastPostNum.txt', mode='rt', encoding='utf-8')
-    lastPostNum = numberFile.read()
-    numberFile.close()
+            print(now.isoformat())
 
     keywords = importSubscribedKeyword()
 
@@ -92,36 +84,21 @@ def activateBot(Log) :
     nowPostNum = element.text
     newPost = int(nowPostNum) - int(lastPostNum)
 
-    # 로그 남기기
-    now = datetime.datetime.now()
-    Log.write("\nDate: " + now.isoformat() + "\n")
-    Log.write("nowPostNum: " + nowPostNum + "\n")
-    Log.write("lastPostNum: " + lastPostNum + "\n")
-    Log.write("newPost: " + str(newPost))
-
     if(newPost > 0):
         for i in range (newPost):
             path1 = '//*[@id="boardList"]/tbody/tr['
             path2 = ']/td[2]/a'
             fullPath = path1 + str(i+5) + path2
             post = driver.find_element_by_xpath(fullPath)
-            Log.write("\n" + post.text + "\n")
             for keyword in keywords:
                 if keyword in post.text:
-                    Log.write(keyword + ", ")
                     sendMessage(post.text, keyword)
 
-    Log.write("\n--------------------------------------------")
-    driver.close()
     return nowPostNum
 
-    #nowTime = now.strftime("%H:%M:%S")
-    #Log.write(("현재 시각: ", nowTime))
-    #nowHour = now.strftime("%H")
+dir = db.reference().child("lastPostNum")
+lastPostNum = dir.get() # 가장 최근에 올라온 게시물 번호
 
-Log = open('Log.txt', mode='at', encoding='utf-8')
-lastPostNum = activateBot(Log)
-numberFile = open('lastPostNum.txt', mode='wt', encoding='utf-8')
-numberFile.write(lastPostNum)
-numberFile.close()
-Log.close()
+# update lastPostNum
+nowPostNum = activateBot(lastPostNum)
+dir.update({"lastPostNum": nowPostNum})
