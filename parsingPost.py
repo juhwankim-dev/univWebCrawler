e6a989d923764bac28a7820865ca5af70e0f65f3
juhwankim-dev <76620764+juhwankim-dev@users.noreply.github.com>
2021-02-17 02:15:14 +0900

    Update: delete selenium, use Post request

--- parsingPost.py
+++ parsingPost.py
@@ -1,7 +1,6 @@
 from collections import OrderedDict
 from time import sleep
 
-from selenium import webdriver
 import datetime
 from pyfcm import FCMNotification
 import firebase_admin
@@ -12,6 +11,7 @@ myInko = inko.Inko()
 import os
 import json
 import random
+import requests
 
 # Firebase database 인증을 위해 ... json 파일을 heroku에 업로드할 수 없기 때문
 cred_json = OrderedDict()
@@ -30,8 +30,6 @@ JSON = json.loads(JSON)
 
 # 링크, 키값 등
 APIKEY = os.environ["APIKEY"]
-DRIVE_LOCATION = '/app/.chromedriver/bin/chromedriver'  # 크롬 드라이버 설치 위치
-CHROME_LOCATION = '/app/.apt/usr/bin/google-chrome'  # 크롬 실행파일 설치 위치
 SITE_URL = "http://www.anyang.ac.kr/bbs/board.do?menuId=23&bsIdx=61&bcIdx=0"
 
 #
@@ -42,23 +40,6 @@ firebase_admin.initialize_app(cred, {
 
 # 파이어베이스 콘솔에서 얻어 온 API키를 넣어 줌
 push_service = FCMNotification(api_key=APIKEY)
-
-# 초기 셋팅
-options = webdriver.ChromeOptions()
-options.add_argument('headless')
-options.add_argument('window-size=1920x1080')
-options.add_argument("disable-gpu")
-# UserAgent값을 바꿔줍시다! (headless 감지를 피하기 위해)
-options.add_argument(
-    "user-agent=Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/61.0.3163.100 Safari/537.36")
-options.add_argument("lang=ko_KR")  # 한국어!
-options.binary_location = CHROME_LOCATION
-chrome_options = options
-
-driver = webdriver.Chrome(DRIVE_LOCATION, options=options)
-driver.get('about:blank')
-driver.execute_script("Object.defineProperty(navigator, 'plugins', {get: function() {return[1, 2, 3, 4, 5];},});")
-driver.execute_script("Object.defineProperty(navigator, 'languages', {get: function() {return ['ko-KR', 'ko']}})")
 
 def importSubscribedKeyword():
     keywords = []
@@ -93,57 +74,39 @@ def sendMessage(title, keyword, url):
     push_service.notify_topic_subscribers(topic_name=keyword, data_message=data_message)
 
 def activateBot() :
-    print("-----------------------------------------------")
-    try:
-        driver.implicitly_wait(10)
-        driver.get(SITE_URL)
-        html = driver.find_element_by_xpath('//*[@id="boardList"]/tbody')
-    except:
-        now = datetime.datetime.now()
-        print("TIMED_OUT_ERROR(Occurrence Time): " + now.isoformat())
-        sendMessage("Timeout error", "모니터링키워드", " ")
-        print("The error message sent to developer")
-        exit()
-
-    # 공지사항이 총 몇개인지 알아보는 작업
-    for index in range(1, 10):
-        try:
-            path = 'tr[' + str(index) + ']/td[1]/span'
-            html.find_element_by_xpath(path).text
-        except:
-            break
+    baseUrl = "http://www.anyang.ac.kr/bbs/boardView.do?bsIdx=61&menuId=23&bcIdx=20&bIdx="
+    datas = {"menuId": "23", "bsIdx": "61", "bcIdx": "0", "page": "1"}
 
     now = datetime.datetime.now()
     print("Date: " + now.isoformat())
+    response = requests.post("http://www.anyang.ac.kr/bbs/ajax/boardList.do", data=datas)
+
+    responseJson = response.json()
+    resultList = responseJson["resultList"]
 
     keywords = importSubscribedKeyword()
-    newPost = ""
+    subject = []
+    bidx = []
 
-    # 키워드를 포함하는 게시물이 있는지 검사한다.
+    # POST 요청 보내서 값 받아오기
+    for notice in resultList:
+        subject.append(notice["SUBJECT"])
+        bidx.append(notice["B_IDX"])
+
+    newPostNumber = ""
     for i in range(10):
-        path = 'tr[' + str(index) + ']/td[2]/a'
-        index = index + 1
-
-        try:
-            post = html.find_element_by_xpath(path).text
-        except:
-            print("NoSuchElement Error")
-            print("-----------------------------------------------")
-            exit()
-
-        newPost = newPost + ", " + post
-        if not post in previousPosts: # 최근 글이 이미 10분 전에 올라왔던 글이라면 새로운글없는거야. not을 붙였으니 새로운글이라면~ 이란 뜻
-            href = html.find_element_by_xpath(path).get_attribute("href")
-            print("title: [" + post + "]")
+        newPostNumber = newPostNumber + ", " + bidx[i]
+        if not bidx[i] in previousPostNumber:  # 최근 10개 게시물중 이 번호가 아닌게 있으면 = 새로운 게시물이면
+            print("title: [" + subject[i] + "]")
             print("contain keyword:", end=" ")
 
             for keyword in keywords:
-                if keyword in post:
+                if keyword in subject[i]:
                     print(keyword, end=", ")
-                    sendMessage(post, keyword, href)
+                    sendMessage(subject[i], keyword, baseUrl + bidx[i])
             print()
 
-    return newPost
+    return newPostNumber
 
 def takeSomeRest():
     rand_value = random.randint(1, 10)
@@ -151,13 +114,12 @@ def takeSomeRest():
 
 now = datetime.datetime.today().weekday()
 time = datetime.datetime.now().strftime('%H')
-if 0 <= now <= 4 and 9 <= int(time) <= 6: # 월~금, 9시~6시 사이에만 작동
-    previousPosts = importPreviousPost()
-    newPost = activateBot()
-    if previousPosts != newPost:
+if 0 <= now <= 4 and 9 <= int(time) <= 18: # 월~금, 9시~6시 사이에만 작동
+    print("-----------------------------------------------")
+    previousPostNumber = importPreviousPost()
+    newPostNumber = activateBot()
+    if previousPostNumber != newPostNumber:
         dir = db.reference().child("previousPosts")
-        dir.update({"previousPosts": newPost})
-        print("\n" + "newPost: " + newPost)
-
-    driver.quit()
+        dir.update({"previousPosts": newPostNumber})
+        print("\n" + "newPost: " + newPostNumber)
     print("-----------------------------------------------")
